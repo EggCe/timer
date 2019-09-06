8b0daf637953710a421810c78fcf1cb51e6ad384
Christian Eckl <ecklc-1995@web.de>
2019-09-06 04:25:21 +0200

    setup layout

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,52 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" app clipped left>
+      <v-divider></v-divider>
 
+      <v-list dense>
+        <v-list-item v-for="programm in programms" :key="programm.id" @click="setView(programm.id)">
+          <v-list-item-icon>
+            <v-icon>{{ programm.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ programm.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Kgl. priv. Schützengilde Straubing</v-toolbar-title>
+    </v-app-bar>
     <v-content>
-      <HelloWorld/>
+      <HelloWorld v-if="page == 1" />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    HelloWorld
   },
   data: () => ({
-    //
+    programms: [
+      { title: "7/3", icon: "arrow-right-bold", id: 1 },
+      { title: "15", icon: "arrow-right-bold", id: 2 },
+      { title: "10/5", icon: "arrow-right-bold", id: 3 }
+    ],
+    drawer: null,
+    page: 0
   }),
+  methods: {
+    setView(id) {
+      this.page = id;
+    }
+  }
 };
 </script>
